61ffd0737a4148e61f10a13e617f05dd231719f7
BitaBig <bita.bigdelu@gmail.com>
2026-09-21 12:15:39 -0700

    Initial commit

---
+++ Assets/Scripts/LevelReset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public GameObject player;
+    public ParticleSystem explosion;
+    
+    public void Reload()
+    {
+        SceneManager.LoadScene(1);   
+    }
+
+    public void GameOver()
+    {
+        player.SetActive(false);
+        Invoke("Reload", 2f);
+        explosion.Play();
+    }
+
+    private void Start()
+    {
+        explosion.Stop();
+    }
+
+    private void FixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
+    }
+}
+
+
